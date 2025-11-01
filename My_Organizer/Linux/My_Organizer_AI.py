115e3158af166a73363bdf5380d69f54dbdaf2ae
Viktor Ilić <vikiilitch@outlook.com>
2025-11-01 23:21:19 +0100

    Add files via upload

--- My_Organizer/Linux/My_Organizer_AI.py
+++ My_Organizer/Linux/My_Organizer_AI.py
@@ -1,15 +1,18 @@
-import g4f, typing, My_Organizer_AI_Interface
+import g4f, typing, My_Organizer_AI_Interface, g4f.Provider
 
 class LLM(My_Organizer_AI_Interface.LLM_Interface):
 
-    LANGUAGE_MODEL: typing.Final[str] = f"gpt-4o-mini"
-
+    LANGUAGE_MODEL: typing.Final[str] = f"gpt-4" 
+    PROVIDER: typing.Final[g4f.Provider] = g4f.Provider.WeWordle
+    
     def __init__(self: typing.Self) -> None:
-        self.ai_client: g4f.Client = g4f.Client()
+       self.language_model: str = self.LANGUAGE_MODEL 
+       self.provider: g4f.Provider = self.PROVIDER
 
     @typing.override
-    def __response_from_AI__(self: typing.Self, prompt: str) -> str:
+    def __response__(self: typing.Self, prompt: str) -> str:
         self.user_query: list[dict[str, str]] = [{f"role": f"user", f"content": prompt}]
+        
+        self.response: str = g4f.ChatCompletion.create(model=self.language_model, provider=self.provider, messages=self.user_query)
 
-        self.response: str = self.ai_client.chat.completions.create(model=self.LANGUAGE_MODEL, messages=self.user_query)
-        return self.response.choices[0].message.content
+        return self.response
